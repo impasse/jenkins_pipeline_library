bfac4d35105fb762c80e9945aa1f8003c64e8a17
shyling <i@shyling.com>
2017-11-22 17:12:40 +0800

    update

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -2,19 +2,23 @@
 
 import com.squareup.okhttp3.*
 
-def call(address, title, url, branch, server, deployer) {
-    def client = new OkHttpClient();
-        def body = RequestBody.create(
-            MediaType.parse("application/json; charset=utf-8"),
-            '''
-            {
-                "text": "**${title}**\\r**URL**: ${url}\\r**Branch**: ${branch}\\r**Server**: ${server}\\r**Deployer**: ${deployer}"
-            }
-            '''
-        )
-        def request = new Request.Builder()
-            .url(address)
-            .post(body)
-            .build()
-        client.newCall(request).execute()
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    def client = new OkHttpClient()
+    def body = RequestBody.create(
+        MediaType.parse("application/json; charset=utf-8"),
+        '''
+        {
+            "text": "**${config.title}**\\r**URL**: ${config.url}\\r**Branch**: ${config.branch}\\r**Server**: ${config.server}\\r**Deployer**: ${config.deployer}"
+        }
+        '''
+    )
+    def request = new Request.Builder()
+        .url(config.address)
+        .post(body)
+        .build()
+    client.newCall(request).execute()
 }
