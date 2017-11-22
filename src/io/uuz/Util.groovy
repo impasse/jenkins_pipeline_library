a57526659e103195091babe2d826ed9e5a51b62f
shyling <i@shyling.com>
2017-11-22 16:26:20 +0800

    update

--- src/io/uuz/Util.groovy
+++ src/io/uuz/Util.groovy
@@ -1,4 +1,7 @@
+@Grab(group='com.squareup.okhttp3', module='okhttp', version='3.9.1')
 package io.uuz;
+
+import com.squareup.okhttp3.*
 
 class Util {
     static def getUserName(self) {
@@ -6,5 +9,23 @@ class Util {
         def build = job.getBuildByNumber(self.env.BUILD_ID as int)
         def userId = build.getCause(Cause.UserIdCause).getUserId()
         return userId
+    }
+
+    @steps
+    static def sendNotification(address, title, url, branch, server, deployer) {
+        def client = new OkHttpClient();
+        def body = RequestBody.create(
+            MediaType.parse("application/json; charset=utf-8"),
+            '''
+            {
+                "text": "**${title}**\\r**URL**: ${url}\\r**Branch**: ${branch}\\r**Server**: ${server}\\r**Deployer**: ${deployer}"
+            }
+            '''
+        )
+        def request = new Request.Builder()
+            .url(address)
+            .post(body)
+            .build()
+        client.newCall(request).execute()
     }
 }
