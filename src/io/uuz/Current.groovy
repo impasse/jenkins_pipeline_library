ccb7ec85dacf4279e244d0280b1514ef46a8d19a
shyling <i@shyling.com>
2017-11-22 16:02:59 +0800

    ...

--- src/io/uuz/Current.groovy
+++ src/io/uuz/Current.groovy
@@ -1,12 +1,9 @@
 package io.uuz;
 
 class Current {
-    Current(script) {
-        this.script = script
-    }
-    def getUserName() {
-        def job = Jenkins.getInstance().getItemByFullName(this.script.env.JOB_BASE_NAME, Job.class)
-        def build = job.getBuildByNumber(this.script.env.BUILD_ID as int)
+    static def getUserName(self) {
+        def job = Jenkins.getInstance().getItemByFullName(self.env.JOB_BASE_NAME, Job.class)
+        def build = job.getBuildByNumber(self.env.BUILD_ID as int)
         def userId = build.getCause(Cause.UserIdCause).getUserId()
         return userId
     }
